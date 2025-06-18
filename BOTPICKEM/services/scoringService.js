c03b9a956641addc0dac074fcf367d0f02ea1744
Cieplyhehe2137 <lukaszciepichal1@gmail.com>
2025-06-18 20:48:20 +0200

    Update scoringService.js

--- BOTPICKEM/services/scoringService.js
+++ BOTPICKEM/services/scoringService.js
@@ -1,82 +1,26 @@
-// services/scoringService.js
-
-const pickemService = require('./pickemService');
-const fs = require('fs');
 const path = require('path');
+const fs = require('fs');
 
+const picksPath = path.join(__dirname, '../data/picks.json');
 const resultsPath = path.join(__dirname, '../data/results.json');
 
+function loadPicks() {
+    if (!fs.existsSync(picksPath)) return {};
+    return JSON.parse(fs.readFileSync(picksPath));
+}
+
 function loadResults() {
-    if (!fs.existsSync(resultsPath)) {
-        return {};
-    }
-    return JSON.parse(fs.readFileSync(resultsPath, 'utf8'));
+    if (!fs.existsSync(resultsPath)) return {};
+    return JSON.parse(fs.readFileSync(resultsPath));
 }
 
-function calculateSwissScore(pick, swissResults) {
-    let score = 0;
-    if (!pick) return score;
-
-    if (pick.threeZero === swissResults.threeZero) score += 4;
-    if (pick.zeroThree === swissResults.zeroThree) score += 4;
-
-    const correctAdvances = pick.advances.filter(team => swissResults.advances.includes(team));
-    score += correctAdvances.length * 2;
-
-    return score;
-}
-
-function calculatePlayoffsScore(pick, playoffsResults) {
-    let score = 0;
-    if (!pick) return score;
-
-    const correctQF = pick.quarterfinals.filter(team => playoffsResults.quarterfinals.includes(team));
-    score += correctQF.length * 2;
-
-    const correctSF = pick.semifinals.filter(team => playoffsResults.semifinals.includes(team));
-    score += correctSF.length * 4;
-
-    if (pick.final === playoffsResults.final) score += 6;
-
-    return score;
-}
-
-function calculateDoubleElimScore(pick, doubleElimResults) {
-    let score = 0;
-    if (!pick) return score;
-
-    const correctUpper = pick.upperfinal.filter(team => doubleElimResults.upperfinal.includes(team));
-    score += correctUpper.length * 3;
-
-    const correctLower = pick.lowerfinal.filter(team => doubleElimResults.lowerfinal.includes(team));
-    score += correctLower.length * 3;
-
-    if (pick.grandfinal === doubleElimResults.grandfinal) score += 6;
-
-    return score;
-}
-
-function calculateTotalScores() {
-    const allPicks = pickemService.getAllPicks();
-    const results = loadResults();
-
-    const totalScores = [];
-
-    for (const userId in allPicks) {
-        const userPicks = allPicks[userId];
-        let total = 0;
-
-        total += calculateSwissScore(userPicks.swiss, results.swiss ?? {});
-        total += calculatePlayoffsScore(userPicks.playoffs, results.playoffs ?? {});
-        total += calculateDoubleElimScore(userPicks.doubleelim, results.doubleelim ?? {});
-
-        totalScores.push({ userId, total });
-    }
-
-    totalScores.sort((a, b) => b.total - a.total);
-    return totalScores;
+function saveScores(fileName, scores) {
+    const scoresPath = path.join(__dirname, `../data/${fileName}`);
+    fs.writeFileSync(scoresPath, JSON.stringify(scores, null, 2));
 }
 
 module.exports = {
-    calculateTotalScores
+    loadPicks,
+    loadResults,
+    saveScores
 };
